1d2b904354eb53c5807e1cab4baaf27a314f17a2
Steffen Kühne <stekhn@gmail.com>
2016-06-07 10:36:48 +0200

    Update readme and gitignore

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Sammlung an Tools um das Parteiprogramm der AFD semantische zu untersuchen. Welc
 ## Verwendung
 1. Repository klonen `git clone https://...`
 2. Erforderliche Module installieren `npm install`
-3. Zum Beispiel `node analyse.js` ausführen, um das Skript zu starten.
+3. Zum Beispiel `node policyIndex.js` ausführen, um das Skript zu starten.
 
-## analyse.js
+## Klassifizierung
+Die Parteiprogramme werden mit Hilfe eines Maschine-Learning-Ansatzes nach eher linken und rechten Aussagen klassifiziert. Zu dem wird untersucht, zu welchem Politikfeld (Außenpolitik, Wirtschaft usw.) eine Aussage gehört. Als Classifier kommt [FIPI](https://github.com/felixbiessmann/fipi/tree/afdHackathon) zum Einsatz. Der Classifier wurde mit Daten aus dem [Manifesto](https://manifestoproject.wzb.eu/)-Projekt trainiert. Manifesto bietet eine strukturierte, wissenschaftliche Analyse verschiedener Parteiprogramme – europaweit und historisch.
+
+## Rechts-Links-Index pro Partei und Politikfeld (policyIndex.js)
 
 ## Verbesserungen
---
+++ policyIndex.js
@@ -0,0 +1,120 @@
+var fs = require('fs');
+var path = require('path');
+
+(function init() {
+
+  loadFile(function (data) {
+
+    data = aggregate(data);
+    data = analyse(data);
+    saveFile('./output/policy.json', JSON.stringify(data, null, 2));
+  });
+})();
+
+function loadFile(callback) {
+
+  fs.readFile('./input/documents-fipi.json', 'utf8', function (error, result) {
+
+    if (!error) {
+
+      callback(JSON.parse(result));
+    } else {
+
+      console.log(error);
+    }
+  });
+}
+
+function aggregate(data) {
+
+  var result = {};
+  var parties = Object.keys(data);
+
+  parties.forEach(function (party) {
+
+    result[party] = {};
+
+    var paragraphs = data[party];
+
+      paragraphs.forEach(function (paragraph) {
+
+        paragraph.fipi.domain.forEach(function (policy) {
+
+          var left = paragraph.fipi.leftright[1].prediction * policy.prediction;
+          var right = paragraph.fipi.leftright[0].prediction * policy.prediction;
+          var boost = policy.prediction;
+
+          if (result[party][policy.label]) {
+
+            result[party][policy.label].left.push(left);
+            result[party][policy.label].right.push(right);
+            result[party][policy.label].boost.push(boost);
+          } else {
+
+            result[party][policy.label] = {
+
+              left: [left],
+              right: [right],
+              boost: [boost]
+            };
+          }
+        });
+      });
+  });
+
+  return result;
+}
+
+function analyse(data) {
+
+  var result = {};
+  var parties = Object.keys(data);
+
+  parties.forEach(function (party) {
+
+    var policies = Object.keys(data[party]);
+
+    policies.forEach(function (policy) {
+
+      var left = getSum(data[party][policy].left);
+      var right = getSum(data[party][policy].right);
+      var boost = getSum(data[party][policy].boost);
+
+      var value = right / boost - left / boost;
+
+      if (!result[policy]) result[policy] = [];
+
+      result[policy].push({
+
+        party: party,
+        value: value
+      });
+    });
+  });
+
+  console.log(result);
+  return result;
+}
+
+function getSum(arr) {
+
+  return arr.reduce(function (a, b) {
+
+    return a + b;
+  });
+}
+
+function saveFile(relativePath, string) {
+
+  relativePath = path.normalize(relativePath);
+
+  console.log('Saved file', relativePath);
+
+  try {
+
+    return fs.writeFileSync(relativePath, string, 'utf8');
+  } catch (error) {
+
+    console.log(error);
+  }
+}
